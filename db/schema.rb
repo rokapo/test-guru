69d2cb7cf5eb65d90f994db694ad9887132903a1
robert491 <bob.pototsky@gmail.com>
2020-12-09 09:00:26 +0300

    fixed migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_03_204358) do
+ActiveRecord::Schema.define(version: 2020_12_09_055743) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -39,9 +39,9 @@ ActiveRecord::Schema.define(version: 2020_12_03_204358) do
     t.string "title", null: false
     t.integer "level", default: 0, null: false
     t.integer "category_id", null: false
-    t.integer "author_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "author_id", null: false
     t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
