e17de3d34045a552373c58185aa0ebf3f9df25c4
robert491 <bob.pototsky@gmail.com>
2021-02-03 18:19:01 +0300

    added controllers

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,45 @@
+class QuestionsController < ApplicationController
+  before_action :find_test,     only: %i[index create]
+  before_action :find_question, only: %i[show destroy]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_record_not_found
+
+  def index
+    @questions = @test.questions
+  end
+
+  def new
+  end
+
+  def create
+    @question = @test.questions.new(question_params)
+    if @question.save
+      redirect_to :test_questions
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @question.destroy
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:body)
+  end
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
+
+  def rescue_with_record_not_found
+    render plain: 'record not found'
+  end
+end
