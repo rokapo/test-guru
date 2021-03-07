16e58971ad0004c4d28dced4f8dfa2b78c0d5e8a
robert491 <bob.pototsky@gmail.com>
2021-03-07 08:56:43 +0300

    auth lesson

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
+require 'digest/sha1'
+
 class User < ApplicationRecord
+
+  include Auth
+
   has_many :test_passages, dependent: :delete_all
   has_many :tests, through: :test_passages, dependent: :delete_all
   has_many :created_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :delete_all
 
-  validates :login, :email, presence: true
+  has_secure_password
 
   def tests_by_level(level = 0)
     tests.where(level: level)
